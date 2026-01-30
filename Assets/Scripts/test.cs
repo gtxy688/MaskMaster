61e08f89a8b2d23198e848b6c01ec7e354ed5b6f
gtxy688 <206379730@qq.com>
2026-01-30 23:11:25 +0800

    fdafa

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    int i;
     // Start is called before the first frame update
     void Start()
     {
